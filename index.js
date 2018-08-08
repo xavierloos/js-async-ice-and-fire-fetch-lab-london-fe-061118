87c3025fc55ba68198100ce93c9514c0f0a93714
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-08-08 16:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ function fetchBooks() {
   //write fetch request to the Game of Thrones API
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(json => renderBooks(json))
+  .then(json => renderBooks(json[4])
+  );
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    const h3 = document.createElement('h3')
+    h3.innerHTML = `<h3>${book.name}</h3>`
+    main.appendChild(h3)
   })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
+  rederBooks();
 })
